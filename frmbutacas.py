95b65789ca4f0058254f2bfb7db10fec4a73d094
salernoe <ernestoasalerno@yahoo.com.ar>
2022-12-17 13:43:26 -0300

    1

--- frmbutacas.py
+++ frmbutacas.py
@@ -5,62 +5,116 @@ class butacas(tk.Toplevel):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.title("butacas")
-        width=472
-        height=163
+        self.title("Cargar butacas")
+        #setting window size
+        width=384
+        height=225
         screenwidth = self.winfo_screenwidth()
         screenheight = self.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         self.geometry(alignstr)
         self.resizable(width=False, height=False)
 
-        GLabel_885=tk.Label(self)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_885["font"] = ft
-        GLabel_885["fg"] = "#333333"
-        GLabel_885["justify"] = "center"
-        GLabel_885["text"] = "Fila"
-        GLabel_885.place(x=10,y=40,width=70,height=25)
+        GLabel_42=tk.Label(self)
+        ft = tkFont.Font(family='Times',size=16)
+        GLabel_42["font"] = ft
+        GLabel_42["fg"] = "#333333"
+        GLabel_42["justify"] = "center"
+        GLabel_42["text"] = "Cargar butacas"
+        GLabel_42.place(x=50,y=10,width=217,height=30)
 
-        GLineEdit_531=tk.Entry(self)
-        GLineEdit_531["borderwidth"] = "1px"
+        GLabel_416=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_531["font"] = ft
-        GLineEdit_531["fg"] = "#333333"
-        GLineEdit_531["justify"] = "center"
-        GLineEdit_531["text"] = ""
-        GLineEdit_531.place(x=70,y=40,width=130,height=30)
+        GLabel_416["font"] = ft
+        GLabel_416["fg"] = "#333333"
+        GLabel_416["justify"] = "center"
+        GLabel_416["text"] = "Id butacas"
+        GLabel_416.place(x=10,y=50,width=70,height=25)
 
-        GLabel_51=tk.Label(self)
+        GLineEdit_416=tk.Entry(self)
+        GLineEdit_416["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_51["font"] = ft
-        GLabel_51["fg"] = "#333333"
-        GLabel_51["justify"] = "center"
-        GLabel_51["text"] = "Numero"
-        GLabel_51.place(x=220,y=40,width=70,height=25)
+        GLineEdit_416["font"] = ft
+        GLineEdit_416["fg"] = "#333333"
+        GLineEdit_416["justify"] = "center"
+        GLineEdit_416["text"] = "Entry"
+        GLineEdit_416.place(x=90,y=50,width=70,height=30)
 
-        GLineEdit_570=tk.Entry(self)
-        GLineEdit_570["borderwidth"] = "1px"
+        GLabel_386=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_570["font"] = ft
-        GLineEdit_570["fg"] = "#333333"
-        GLineEdit_570["justify"] = "center"
-        GLineEdit_570["text"] = ""
-        GLineEdit_570.place(x=290,y=40,width=130,height=30)
+        GLabel_386["font"] = ft
+        GLabel_386["fg"] = "#333333"
+        GLabel_386["justify"] = "center"
+        GLabel_386["text"] = "Fila"
+        GLabel_386.place(x=180,y=50,width=70,height=25)
 
-        GLabel_192=tk.Label(self)
+        GLineEdit_470=tk.Entry(self)
+        GLineEdit_470["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_192["font"] = ft
-        GLabel_192["fg"] = "#333333"
-        GLabel_192["justify"] = "center"
-        GLabel_192["text"] = "Disponible"
-        GLabel_192.place(x=130,y=100,width=70,height=25)
+        GLineEdit_470["font"] = ft
+        GLineEdit_470["fg"] = "#333333"
+        GLineEdit_470["justify"] = "center"
+        GLineEdit_470["text"] = "Entry"
+        GLineEdit_470.place(x=250,y=50,width=80,height=30)
 
-        GLineEdit_564=tk.Entry(self)
-        GLineEdit_564["borderwidth"] = "1px"
+        GLabel_643=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_564["font"] = ft
-        GLineEdit_564["fg"] = "#333333"
-        GLineEdit_564["justify"] = "center"
-        GLineEdit_564["text"] = ""
-        GLineEdit_564.place(x=210,y=100,width=130,height=30)
+        GLabel_643["font"] = ft
+        GLabel_643["fg"] = "#333333"
+        GLabel_643["justify"] = "center"
+        GLabel_643["text"] = "Numero de  Butaca"
+        GLabel_643.place(x=10,y=100,width=70,height=25)
+
+        GLineEdit_930=tk.Entry(self)
+        GLineEdit_930["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        GLineEdit_930["font"] = ft
+        GLineEdit_930["fg"] = "#333333"
+        GLineEdit_930["justify"] = "center"
+        GLineEdit_930["text"] = "Entry"
+        GLineEdit_930.place(x=90,y=100,width=70,height=30)
+
+        GLabel_666=tk.Label(self)
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_666["font"] = ft
+        GLabel_666["fg"] = "#333333"
+        GLabel_666["justify"] = "center"
+        GLabel_666["text"] = "Id de sala"
+        GLabel_666.place(x=180,y=100,width=70,height=25)
+
+        GListBox_195=tk.Listbox(self)
+        GListBox_195["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        GListBox_195["font"] = ft
+        GListBox_195["fg"] = "#333333"
+        GListBox_195["justify"] = "center"
+        GListBox_195.place(x=250,y=100,width=80,height=30)
+
+        GButton_563=tk.Button(self)
+        GButton_563["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times',size=10)
+        GButton_563["font"] = ft
+        GButton_563["fg"] = "#000000"
+        GButton_563["justify"] = "center"
+        GButton_563["text"] = "Aceptar"
+        GButton_563.place(x=140,y=170,width=70,height=25)
+        GButton_563["command"] = self.GButton_563_command
+
+        GButton_515=tk.Button(self)
+        GButton_515["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times',size=10)
+        GButton_515["font"] = ft
+        GButton_515["fg"] = "#000000"
+        GButton_515["justify"] = "center"
+        GButton_515["text"] = "Cancelar"
+        GButton_515.place(x=230,y=170,width=70,height=25)
+        GButton_515["command"] = self.GButton_515_command
+
+    def GButton_563_command(self):
+        print("command")
+
+
+    def GButton_515_command(self):
+        print("command")
+
+
--- frmreservas.py
+++ frmreservas.py
@@ -6,76 +6,132 @@ class reservas(tk.Toplevel):
         super().__init__(master)
         self.master = master
         self.title("Reservas")
-        width=483
-        height=163
+        #setting window size
+        width=440
+        height=312
         screenwidth = self.winfo_screenwidth()
         screenheight = self.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         self.geometry(alignstr)
         self.resizable(width=False, height=False)
 
-        GLabel_669=tk.Label(self)
+        GLabel_404=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_669["font"] = ft
-        GLabel_669["fg"] = "#333333"
-        GLabel_669["justify"] = "center"
-        GLabel_669["text"] = "Precio"
-        GLabel_669.place(x=20,y=20,width=70,height=25)
+        GLabel_404["font"] = ft
+        GLabel_404["fg"] = "#333333"
+        GLabel_404["justify"] = "center"
+        GLabel_404["text"] = "Hola!!!, por favor Carga tu reserva  no te olvides  llenar todos los campos "
+        GLabel_404.place(x=10,y=10,width=404,height=39)
 
-        GLineEdit_909=tk.Entry(self)
-        GLineEdit_909["borderwidth"] = "1px"
+        GLabel_275=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_909["font"] = ft
-        GLineEdit_909["fg"] = "#333333"
-        GLineEdit_909["justify"] = "center"
-        GLineEdit_909["text"] = "Entry"
-        GLineEdit_909.place(x=80,y=20,width=110,height=30)
+        GLabel_275["font"] = ft
+        GLabel_275["fg"] = "#333333"
+        GLabel_275["justify"] = "center"
+        GLabel_275["text"] = "Usuarios"
+        GLabel_275.place(x=20,y=80,width=70,height=25)
 
-        GLabel_364=tk.Label(self)
+        GLineEdit_306=tk.Entry(self)
+        GLineEdit_306["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_364["font"] = ft
-        GLabel_364["fg"] = "#333333"
-        GLabel_364["justify"] = "center"
-        GLabel_364["text"] = "Fecha"
-        GLabel_364.place(x=210,y=20,width=70,height=25)
+        GLineEdit_306["font"] = ft
+        GLineEdit_306["fg"] = "#333333"
+        GLineEdit_306["justify"] = "center"
+        GLineEdit_306["text"] = "Entry"
+        GLineEdit_306.place(x=90,y=80,width=90,height=30)
 
-        GListBox_165=tk.Listbox(self)
-        GListBox_165["borderwidth"] = "1px"
+        GLabel_258=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GListBox_165["font"] = ft
-        GListBox_165["fg"] = "#333333"
-        GListBox_165["justify"] = "center"
-        GListBox_165.place(x=290,y=20,width=110,height=30)
+        GLabel_258["font"] = ft
+        GLabel_258["fg"] = "#333333"
+        GLabel_258["justify"] = "center"
+        GLabel_258["text"] = "Fecha"
+        GLabel_258.place(x=190,y=80,width=70,height=25)
 
-        GLabel_348=tk.Label(self)
+        GLineEdit_362=tk.Entry(self)
+        GLineEdit_362["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_348["font"] = ft
-        GLabel_348["fg"] = "#333333"
-        GLabel_348["justify"] = "center"
-        GLabel_348["text"] = "Butaca"
-        GLabel_348.place(x=10,y=80,width=70,height=25)
+        GLineEdit_362["font"] = ft
+        GLineEdit_362["fg"] = "#333333"
+        GLineEdit_362["justify"] = "center"
+        GLineEdit_362["text"] = "Entry"
+        GLineEdit_362.place(x=260,y=80,width=90,height=30)
 
-        GListBox_661=tk.Listbox(self)
-        GListBox_661["borderwidth"] = "1px"
+        GLabel_961=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GListBox_661["font"] = ft
-        GListBox_661["fg"] = "#333333"
-        GListBox_661["justify"] = "center"
-        GListBox_661.place(x=80,y=80,width=110,height=30)
+        GLabel_961["font"] = ft
+        GLabel_961["fg"] = "#333333"
+        GLabel_961["justify"] = "center"
+        GLabel_961["text"] = "Butaca"
+        GLabel_961.place(x=20,y=140,width=70,height=25)
 
-        GLabel_420=tk.Label(self)
+        GListBox_8=tk.Listbox(self)
+        GListBox_8["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_420["font"] = ft
-        GLabel_420["fg"] = "#333333"
-        GLabel_420["justify"] = "center"
-        GLabel_420["text"] = "Descuento"
-        GLabel_420.place(x=210,y=80,width=70,height=25)
+        GListBox_8["font"] = ft
+        GListBox_8["fg"] = "#333333"
+        GListBox_8["justify"] = "center"
+        GListBox_8.place(x=90,y=140,width=90,height=30)
 
-        GListBox_764=tk.Listbox(self)
-        GListBox_764["borderwidth"] = "1px"
+        GLabel_332=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GListBox_764["font"] = ft
-        GListBox_764["fg"] = "#333333"
-        GListBox_764["justify"] = "center"
-        GListBox_764.place(x=290,y=80,width=110,height=30)
+        GLabel_332["font"] = ft
+        GLabel_332["fg"] = "#333333"
+        GLabel_332["justify"] = "center"
+        GLabel_332["text"] = "Descuento"
+        GLabel_332.place(x=190,y=140,width=70,height=25)
+
+        GLineEdit_228=tk.Entry(self)
+        GLineEdit_228["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        GLineEdit_228["font"] = ft
+        GLineEdit_228["fg"] = "#333333"
+        GLineEdit_228["justify"] = "center"
+        GLineEdit_228["text"] = "Entry"
+        GLineEdit_228.place(x=260,y=140,width=90,height=30)
+
+        GLabel_433=tk.Label(self)
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_433["font"] = ft
+        GLabel_433["fg"] = "#333333"
+        GLabel_433["justify"] = "center"
+        GLabel_433["text"] = "Tarjeta"
+        GLabel_433.place(x=120,y=190,width=70,height=25)
+
+        GLineEdit_666=tk.Entry(self)
+        GLineEdit_666["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        GLineEdit_666["font"] = ft
+        GLineEdit_666["fg"] = "#333333"
+        GLineEdit_666["justify"] = "center"
+        GLineEdit_666["text"] = "Entry"
+        GLineEdit_666.place(x=190,y=190,width=90,height=30)
+
+        GButton_517=tk.Button(self)
+        GButton_517["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times',size=10)
+        GButton_517["font"] = ft
+        GButton_517["fg"] = "#000000"
+        GButton_517["justify"] = "center"
+        GButton_517["text"] = "Aceptar"
+        GButton_517.place(x=190,y=260,width=70,height=25)
+        GButton_517["command"] = self.GButton_517_command
+
+        GButton_779=tk.Button(self)
+        GButton_779["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times',size=10)
+        GButton_779["font"] = ft
+        GButton_779["fg"] = "#000000"
+        GButton_779["justify"] = "center"
+        GButton_779["text"] = "Cancelar"
+        GButton_779.place(x=280,y=260,width=70,height=25)
+        GButton_779["command"] = self.GButton_779_command
+
+    def GButton_517_command(self):
+        print("command")
+
+
+    def GButton_779_command(self):
+        print("command")
+
 
